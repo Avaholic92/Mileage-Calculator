9f1d1728c51d74f0b114f2b591e6cb0b8dcdaadd
Avaholic92 <coloradoavalanche1992@gmail.com>
2017-08-01 14:58:27 -0500

    initial commit

---
+++ milecalc.py
@@ -0,0 +1,28 @@
+#!python
+#
+# mileage reimbursement calculator
+#
+
+#imports
+import math
+
+
+def main():
+	miles = float (input('Total miles driven: '))
+	rate = float(.535)
+	calc = (miles * rate) 
+	total = round(calc)
+	print ('Your reimbursement total is: $',total)  
+    
+if __name__ == "__main__":
+  main()
+
+##version debug info
+import platform
+print ()
+print ()
+print ('P Y T H O N  D E B U G')
+print ('----------------------')
+print ('Current platform is: ', (platform.python_version()))
+print ('Running on:', (platform.platform()))
+
